44df447a72a9462586311af95b4a6787bdd922bf
Johan Rova <johan.rova@protonmail.com>
2021-09-16 09:32:06 +0200

    now using bootstrap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,16 @@
 <template>
     <h1>HELLO!</h1>
     <h1>{{this.$store.state.name}}</h1>
+
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+
+  With Bootstrap!
+
+  <button type="button" class="btn btn-warning">
+
+    <span aria-hidden="true">&times;</span>
+
+  </button>
+
+</div>
 </template>
