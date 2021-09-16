7292aa3a0504dff80e07e4d5c045c95dfc564ef0
Johan Rova <johan.rova@protonmail.com>
2021-09-16 13:51:27 +0200

    Made a rough skeleton of how the layour should be

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HelloWorld from '/src/components/HelloWorld.vue'
+import home from '/src/views/home.vue'
 
 const routes = [
     {
         path: '/HelloWorld',
         name: 'Hello world!',
         component: HelloWorld,
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: home,
     },
 ]
 
---
+++ src/views/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 style="position: absolute; top: 0; width: 100%">Musicalez</h1>
+  
+  <div class="input-group mb-3" style="padding-left:3vw;padding-right:3vw">
+    <div class="input-group-prepend">
+    </div>
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Search"
+      aria-label="Search"
+      aria-describedby="basic-addon1"
+    />
+  </div>
+
+  <div class="d-flex flex-row justify-content-center">
+    <button type="button" class="btn btn-primary" style="margin-left:0.5vw;margin-right:0.5vw">Search song</button>
+    <button type="button" class="btn btn-primary" style="margin-left:0.5vw;margin-right:0.5vw">Search artist</button>
+    <button type="button" class="btn btn-primary" style="margin-left:0.5vw;margin-right:0.5vw">Search album</button>
+  </div>
+
+  <div class="fixed-bottom border border-4" style="height:15vh">
+      <div>
+
+          <h1>lol</h1>
+      </div>
+  </div>
+</template>
