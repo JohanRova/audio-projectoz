45ab40280676609cc2eda7c4261661d1caeca4c6
Johan Rova <johan.rova@protonmail.com>
2021-09-17 10:35:03 +0200

    added some icons in the media controller, added a placeholder method for search to test api

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,8 @@
 <template>
   <h1 style="position: absolute; top: 0; width: 100%">Musicalez</h1>
-  
-  <div class="input-group mb-3" style="padding-left:3vw;padding-right:3vw">
-    <div class="input-group-prepend">
-    </div>
+
+  <div class="input-group mb-3" style="padding-left: 3vw; padding-right: 3vw">
+    <div class="input-group-prepend"></div>
     <input
       type="text"
       class="form-control"
@@ -14,16 +13,60 @@
   </div>
 
   <div class="d-flex flex-row justify-content-center">
-    <button type="button" class="btn btn-primary" style="margin-left:0.5vw;margin-right:0.5vw">Search song</button>
-    <button type="button" class="btn btn-primary" style="margin-left:0.5vw;margin-right:0.5vw">Search artist</button>
-    <button type="button" class="btn btn-primary" style="margin-left:0.5vw;margin-right:0.5vw">Search album</button>
+    <button
+      type="button"
+      class="btn btn-primary"
+      style="margin-left: 0.5vw; margin-right: 0.5vw"
+    >
+      Search song
+    </button>
+    <button
+      type="button"
+      class="btn btn-primary"
+      style="margin-left: 0.5vw; margin-right: 0.5vw"
+    >
+      Search artist
+    </button>
+    <button
+      type="button"
+      class="btn btn-primary"
+      style="margin-left: 0.5vw; margin-right: 0.5vw"
+    >
+      Search album
+    </button>
   </div>
 
-  <div class="fixed-bottom border border-4" style="height:15vh">
-      <div>
-          <h2>Controller</h2>
-          
-          
+  <div class="fixed-bottom border border-4" style="height: 15vh">
+    <div>
+      <div class="d-flex justify-content-center" style="padding-top:1vh">
+        <button type="button" class="btn btn-outline-primary mx-1" style="display:flex;">
+          <img src="/src/icons/skip-backward.svg"/>
+        </button>
+        <button type="button" class="btn btn-outline-primary mx-1" style="display:flex;">
+          <img src="/src/icons/play.svg"/>
+        </button>
+        <button type="button" class="btn btn-outline-primary mx-1" style="display:flex">
+          <img src="/src/icons/pause.svg"/>
+        </button>
+        <button type="button" class="btn btn-outline-primary mx-1" style="display:flex;">
+          <img src="/src/icons/skip-forward.svg"/>
+        </button>
       </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    
+  },
+  methods:{
+    async searchArtist(){
+      let rawResponse = await fetch('https://yt-music-api.herokuapp.com/api/yt/artists/metallica')
+      console.log(await rawResponse.json())
+    }
+  }
+
+}
+</script>
