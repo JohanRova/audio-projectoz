f7c39961dd3f80e0c3677bfe65ab6b86bfe0f85b
Johan Rova <johan.rova@protonmail.com>
2021-09-23 18:52:17 +0200

    songresult component seems to be working now with router-view, media controller works with it, can copy link and routing works like intended

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-<Search/>
-<router-view/>
-<SongResult/>
-<MediaController/>
+  <h1 style="position: absolute; top: 0; width: 100%">
+    <a href="/">Musicalez</a>
+  </h1>
+  <div class="input-group mb-3" style="padding-left: 3vw; padding-right: 3vw">
+    <div class="input-group-prepend"></div>
+    <input
+      v-model="searchterm"
+      type="text"
+      class="form-control"
+      placeholder="Search"
+      aria-label="Search"
+      aria-describedby="basic-addon1"
+    />
+  </div>
+  <!--buttons for search-->
+  <div class="d-flex flex-row justify-content-center">
+  <router-link tag="button" class="btn btn-primary" :to="'/song' + '/metallica'" >Search song</router-link>
+    <!-- <button
+      v-on:click="searchSong(searchterm)"
+      type="button"
+      class="btn btn-primary"
+      style="margin-left: 0.5vw; margin-right: 0.5vw"
+    > 
+      Search song
+    </button> -->
+    <button
+      v-on:click="searchArtist(searchterm)"
+      type="button"
+      class="btn btn-primary"
+      style="margin-left: 0.5vw; margin-right: 0.5vw"
+    >
+      Search artist
+    </button>
+  </div>
+  <router-view />
+  <!-- <SongResult /> -->
+  <MediaController />
 </template>
 
 <script>
 import MediaController from "./components/MediaController.vue";
-import Search from "./components/Search.vue"
-import SongResult from "./components/SongResult.vue"
+import Search from "./components/Search.vue";
+import SongResult from "./components/SongResult.vue";
 
 export default {
-  components:{
+  mounted(){
+    console.log(this.$route.params.id)
+  },
+  components: {
     MediaController,
     Search,
     SongResult,
   },
-  methods:
-  {
+  methods: {
+    async searchArtist(searchFor) {
+      let rawResponse = await fetch(
+        "https://yt-music-api.herokuapp.com/api/yt/artists/" + searchFor
+      );
+      this.$store.commit("setSearchArtist", await rawResponse.json());
+      console.log(this.$store.state.searchResultArtist);
+    },
+    async searchSong(searchFor) {
+      let rawResponse = await fetch(
+        "https://yt-music-api.herokuapp.com/api/yt/songs/" + searchFor
+      );
+      this.$store.commit("setSearchSong", await rawResponse.json());
+      console.log(this.$store.state.searchResultSong);
+    },
   },
-}
-
+};
 </script>
 
 <style>
--- src/components/SongResult.vue
+++ src/components/SongResult.vue
@@ -1,56 +1,72 @@
 <template>
-    <div class="d-inline-flex" style="margin-top:2vh">
-        <ol
-        id="array-rendering"
-        class="list-group"
-        style="width: 95vw; margin-bottom: 19vh"
-        >
-        <li
-            class="list-group-item d-flex flex-column"
-            v-for="(item, index) in this.$store.state.searchResultSong.content"
-            v-bind:key="item.id"
-        >
-            <div class="d-flex flex-row justify-content-between">
-            <button
-                v-on:click="play(item.videoId, index)"
-                type="button"
-                class="btn btn-outline-primary mx-1"
-                style="display: flex"
-            >
-                <img src="/src/icons/play.svg" />
-            </button>
-            <div class="p-2 border border-primary rounded mx-2 w-50">
-                {{ item.name }}
-            </div>
-            <div class="p-2 border border-primary rounded mx-2 w-50">
-                {{ item.artist.name }}
-            </div>            
-            <button
-                data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top" data-bs-custom-class="border"
-                v-on:click="copyShareLink(item.videoId, 'song' )"
-                type="button"
-                class="btn btn-outline-primary mx-1"
-                style="display: flex"
-            >
-                <img src="/src/icons/share.svg" />
-            </button>
-            </div>
-        </li>
-        </ol>
-    </div>
+  <div class="d-inline-flex" style="margin-top: 2vh">
+    <ol
+      id="array-rendering"
+      class="list-group"
+      style="width: 95vw; margin-bottom: 19vh"
+    >
+      <li
+        class="list-group-item d-flex flex-column"
+        v-for="(item, index) in this.$store.state.searchResultSong.content"
+        v-bind:key="item.id"
+      >
+        <div class="d-flex flex-row justify-content-between">
+          <button
+            v-on:click="play(item.videoId, index)"
+            type="button"
+            class="btn btn-outline-primary mx-1"
+            style="display: flex"
+          >
+            <img src="/src/icons/play.svg" />
+          </button>
+          <div class="p-2 border border-primary rounded mx-2 w-50">
+            {{ item.name }}
+          </div>
+          <div class="p-2 border border-primary rounded mx-2 w-50">
+            {{ item.artist.name }}
+          </div>
+          <button
+            data-bs-toggle="tooltip"
+            data-bs-placement="top"
+            title="Tooltip on top"
+            data-bs-custom-class="border"
+            v-on:click="copyShareLink(item.videoId, 'song')"
+            type="button"
+            class="btn btn-outline-primary mx-1"
+            style="display: flex"
+          >
+            <img src="/src/icons/share.svg" />
+          </button>
+        </div>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        play(id, index){
-        console.log(id)
-        window.player.loadVideoById(id)
-        window.player.playVideo()
-        this.$store.commit("setPlaylist", index)
-        console.log(this.$store.state.currentlyPlayingIndex)
-        },
-    
+  mounted(){
+      this.searchSong(this.$route.params.songId)
+      
+  },
+  methods: {
+    play(id, index) {
+      console.log(id);
+      window.player.loadVideoById(id);
+      window.player.playVideo();
+      this.$store.commit("setPlaylist", index);
+      console.log(this.$store.state.currentlyPlayingIndex);
+    },
+    async searchSong(searchFor) {
+      let rawResponse = await fetch(
+        "https://yt-music-api.herokuapp.com/api/yt/songs/" + searchFor
+      );
+      this.$store.commit("setSearchSong", await rawResponse.json());
+      console.log(this.$store.state.searchResultSong);
+    },
+      copyShareLink(videoId, shareType){
+      navigator.clipboard.writeText("localhost:3000/" + shareType + "/" + videoId)
     }
-}
+  },
+};
 </script>
